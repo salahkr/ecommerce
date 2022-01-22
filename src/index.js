c1aabc0e70cbd607c8c87a21167f4f37cd231db0
salah <krenbhsalah@gmsil,com>
2022-01-22 18:53:52 +0300

    search page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
 import '@fortawesome/fontawesome-free/js/all.min';
+import 'webpack-jquery-ui';
+import 'webpack-jquery-ui/css';
+import 'jquery-ui-touch-punch/jquery.ui.touch-punch';
 
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
@@ -122,5 +125,19 @@ $(document).ready(function() {
 
         // بدل معلومات بطاقة الائتمان بين الظهور والاخفاء
         $('#credit-card-info').toggle();
+    });
+
+    // مكون البحث حسب السعر 
+    $("#price-range").slider({
+        range: true,
+        min: 50,
+        max: 1000,
+        step: 50,
+        values: [ 250, 800 ],
+        slide: function(event, ui) {
+            $('#price-min').text(ui.values[0]);
+            $('#price-max').text(ui.values[1]);
+        }
+        
     });
 });
